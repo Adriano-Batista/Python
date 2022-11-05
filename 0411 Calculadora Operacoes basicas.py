c468701afbcba1d4ed1c2c9a3c5b2e5b823c4858
Adriano-Batista <111244908+Adriano-Batista@users.noreply.github.com>
2022-11-04 21:29:37 -0300

    Python calculator basic operations
    First algorithm with IF statements in python. It's calculates 2 numbers using the 4 basics math operations

---
+++ 0411 Calculadora Operacoes basicas.py
@@ -0,0 +1,32 @@
+#Variáveis
+res = float
+
+print("="*40)
+print("{:^40}" .format("Calculadora de operações básicas"))
+print("="*40)
+
+n1 = float (input("Digite o primeiro número: "))
+op = input("A operação: ")
+n2 = float (input("Digite o segundo número: "))
+
+if op == "+":
+    res = n1+n2
+    print(res)
+
+
+
+if op == "-":
+    res = n1-n2
+    print(res)
+
+if op == "*":
+    res = n1*n2
+    print(res)
+
+if op == "/":
+    res = n1/n2
+    print(res)
+
+if res = ".0"
+    res = int
+    print(res)
