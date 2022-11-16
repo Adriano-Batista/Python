1eaeddda7ac018a4dc04f9fcb6ff5be7a8cfcd5d
Adriano-Batista <111244908+Adriano-Batista@users.noreply.github.com>
2022-11-16 20:58:54 -0300

    Adivinhe o Número
    Um algoritmo onde o computador escolhe aleatoriamente um número de 0 a 10 e o jogador deve adivinhar qual número foi escolhido. Após o jogador acertar é apresentado o número de palpites que o jogador precisou

---
+++ 1611 Adivinhe_o_numero.py
@@ -0,0 +1,38 @@
+import random
+
+#header
+print("="*40)
+print("{:^40}" .format("Adivinhação Numerada"))
+print("="*40)
+print("{:^40}" .format("Adivinhe qual o número, de 0 a 10\n"))
+
+#escolha do jogador
+resposta = str(input("Deseja jogar [S/N]? ")) .upper() .split()[0]
+
+#tratamento de erro
+while resposta not in "SN":
+    print("RESPOSTA INVÁLIDA!")
+    resposta = str(input("Deseja jogar [S/N]? ")) .upper() .split()[0]
+
+
+numero = random.randint(0,11)
+
+print("="*40)
+print("{:^40}" .format("PREPARE-SE..."))
+print("="*40)
+
+#escolha_do_jogador = int(input("Qual o número que você escolheu? "))
+escolha_do_jogador = None
+i = 0
+
+while escolha_do_jogador != numero:
+
+    escolha_do_jogador = int(input("Qual o número que você escolheu? "))
+    i += 1
+
+    if escolha_do_jogador == numero:
+        print(f"Parabéns, você acertou! \n Número de tentativas: [{i}]")
+
+    else:
+
+        print("Resposta errada...\n Tente mais uma vez")
