ffe8548c59051b5a7152093a831b9807c619092c
Adriano-Batista <111244908+Adriano-Batista@users.noreply.github.com>
2022-11-24 21:13:59 -0300

    Encurtando o código
    Atualizações para encurtar o código e também a função de escolher aleatóriamente entre uma lista de palavras a próxima palavra do jogo

--- 2211 jogoDaForca.py
+++ 2211 jogoDaForca.py
@@ -2,29 +2,30 @@
 O programa deve ter uma escolha de palavras em uma lista ou apenas 1 palavra;
 O usuário pode errar apenas 6 vezes, após isso é game over
 Obs: Não é permitido o uso de listas ao menos que para a escolha das palavras!'''
+from random import choice
 
-from dataclasses import replace
+palavras = ('ABACAXI', 'MANDIOCA', 'JIBOIA', 'VIAGEM', 'GREENWICH', 'MARAVILHOSA', 'MARTE')
 
-palavraEscolhida = str("abacaxi").upper() .strip()
+palavraEscolhida = choice(palavras)
 
-print("="*40)
-print("{:^40}" .format("JOGO DA FORCA"))
-print("{:^40}" .format("SEJA BEM-VINDO"))
-print("="*40)
+p = 40
+print("="*p)
+print(f"{'JOGO DA FORCA':^40}")
+print(f"{'SEJA BEM-VINDO':^40}")
+print("="*p)
 
-lerRegras = str(input("Deseja ler as regras? ")) .upper().strip()[0]
+lerRegras = str(input("Deseja ler as regras? [S/N] ")).upper().strip()[0]
 
-while lerRegras not in ("SN"):
+while lerRegras not in "SN":
     print("Resposta inválida!")
-    lerRegras = str(input("Deseja ler as regras? ")) .upper().strip()[0]
-
+    lerRegras = str(input("Deseja ler as regras? [S/N] ")).upper().strip()[0]
 
 if lerRegras == "S":
     print("Você tem 6 chances para acertar a palavra \nPara cada erro seu contador de erros subirá, chegando em 6 você perde. \n")
 
-print("="*40)
-print("{:^40}" .format("VAMOS JOGAR!"))
-print("="*40)
+print("="*p)
+print(f"{'VAMOS JOGAR!':^40}")
+print("="*p)
 
 erros = 0
 
@@ -46,31 +47,3 @@ while True:
     if escolhaLetra not in palavraEscolhida:
         erros += 1
         print(f"Você errou!\nErros = {erros}")
-    
-    
-    
-    
-    
-    
-    
-    
-    #Códigos perdidos no limbo pra excluir depois
-    
-    '''print(f"A palavra escolhida possui {len(palavraEscolhida)} letras")
-    palavraEscolhidaAlterada = (palavraEscolhida.replace(palavraEscolhida, "_ "*7))
-    print(palavraEscolhidaAlterada)
-    #print(palavraEscolhida.replace(palavraEscolhida, "_", len(palavraEscolhida)))'''
-
-    '''if escolhaLetra in palavraEscolhida:
-        escolhaLetra.replace(palavraEscolhidaAlterada, escolhaLetra)
-        print(escolhaLetra)'''
-
-    '''palavraEscolhida.find(escolhaLetra)
-    if palavraEscolhida.find != -1:
-        palavraEscolhida.split()[0:8]
-        print(palavraEscolhida.replace(palavraEscolhidaAlterada, escolhaLetra))
-        #print(palavraEscolhida.replace(palavraEscolhidaAlterada, escolhaLetra))'''
-
-
-
-#.upper().split()[0:8]
