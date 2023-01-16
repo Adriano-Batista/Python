ef5a5e9ae8447aeb8c6873560c88a80b441e190a
Adriano-Batista <111244908+Adriano-Batista@users.noreply.github.com>
2023-01-16 20:13:18 -0300

    Validador_CPF

---
+++ ValidarCPF/Lib/calculations/calculations.py
@@ -0,0 +1,3 @@
+import main
+
+MULTIPLY = 10
---
+++ ValidarCPF/main.py
@@ -0,0 +1,83 @@
+#from Lib.calculations import calculations
+
+#Função remove os pontos e traços do CPF digitado pelo usuário
+def punctuation_replace(local_user_cpf):
+    if '.' in local_user_cpf or '-' in local_user_cpf:
+        local_user_cpf = user_cpf.replace('.','')
+        local_user_cpf = user_cpf.replace('-','')
+
+#Função para validar se o CPF do usuário é do tipo numérico e possui 11 dígitos (padrão do CPF)
+def numeric_error_handling(num_user_cpf):
+    while not num_user_cpf.isnumeric() or len(num_user_cpf) != 11:
+        num_user_cpf = (input('Erro! Digite um valor correto: '))
+
+
+#constantes da validação do penúltimo número do CPF
+MULTIPLY_VALIDATION_1 = 10
+CPF_DIGIT_SUM_1 = 0
+
+
+#função que multiplica os 9 primeiros dígitos de um CPF de forma decrescente de 10 até 2
+def multiply_CPF_digits(cpf_first_digits, MULTIPLIER, DIGIT_SUM):
+    for cpf_digit in cpf_first_digits:
+        results = int(cpf_digit)*MULTIPLIER
+        MULTIPLIER -= 1
+        DIGIT_SUM += results
+    
+'''local_division_rest == CPF_DIGIT_SUM_1%11
+return local_division_rest'''
+
+
+#Validador de CPF
+while True:
+    user_cpf = input('Digite seu CPF: ')
+    
+#Execução da função que remove os pontos e traços do CPF digitado pelo usuário
+    punctuation_replace(user_cpf)
+
+##Execução da função para validar se o CPF do usuário é do tipo numérico e possui 11 dígitos (padrão do CPF)
+    numeric_error_handling(user_cpf)
+
+#Descobre os 9 primeiros dígitos do CPF do usuário
+    cpf_first_9_digits = user_cpf[:9]
+
+#função que multiplica os 9 primeiros dígitos de um CPF de forma decrescente de 10 até 2
+    multiply_CPF_digits(cpf_first_9_digits, MULTIPLY_VALIDATION_1, CPF_DIGIT_SUM_1)
+
+    #Validação do penúltimo dígito do CPF do usuário
+    if division_rest < 2:
+        validated_cpf_number = 0
+    else:
+        digit_verify = 11
+        digit_result = digit_verify - division_rest
+        validated_cpf_number = digit_result
+
+    #constantes da validação do último número do CPF
+    MULTIPLY_VALIDATION_2 = 11
+    CPF_DIGIT_SUM_2 = 0
+    
+    cpf_first_10_digits = cpf_first_9_digits + str(validated_cpf_number)
+    #função que multiplica os 9 primeiros dígitos de um CPF de forma decrescente de 10 até 2
+    for cpf_digit in cpf_first_10_digits:
+        results = int(cpf_digit)*MULTIPLY_VALIDATION_2
+        MULTIPLY_VALIDATION_2 -= 1
+        CPF_DIGIT_SUM_2 += results
+    
+    division_rest_2 = CPF_DIGIT_SUM_2%11
+
+    #Validação do último dígito do CPF do usuário
+    if division_rest_2 < 2:
+        validated_second_cpf_number = 0
+    else:
+        second_digit_verify = 11
+        second_digit_result = second_digit_verify - division_rest_2
+        validated_second_cpf_number = second_digit_result
+
+    validated_cpf = user_cpf[:9] + str(validated_cpf_number) + str(validated_second_cpf_number)
+
+    if validated_cpf == user_cpf:
+        print('CPF VÁLIDO.')
+    else:
+        print('CPF INVÁLIDO.')
+    break
+print(validated_cpf)
